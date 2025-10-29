f45d0c8e0da4842e0a5149c656e4f464e4e333a5
veleswaran <vels344@gmail.com>
2025-10-29 16:30:34 +0530

    resolve deploy issue

--- src/Layout/Navbar.js
+++ src/Layout/Navbar.js
@@ -1,6 +1,6 @@
 import "./Navbar.css";
 import { useEffect, useState } from "react"
-import { useNavigate } from "react-router-dom"
+import { useNavigate,Link } from "react-router-dom"
 
 export default function Navbar() {
   let [user, setUser] = useState(null)
